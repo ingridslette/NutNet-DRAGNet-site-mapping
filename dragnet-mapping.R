b42cd3963501d7e42d72924ff0a4d636a4ceb48b
ingridslette <ingrid.slette@gmail.com>
2026-07-08 12:53:50 -0500

    updated with newest data and re-ran

--- dragnet-mapping.R
+++ dragnet-mapping.R
@@ -3,7 +3,7 @@ library(ggmap)
 
 register_stadiamaps("98dda47f-a35b-4ead-aa46-dda02c95d912", write = FALSE)
 
-sites <- read.csv("/Users/ingridslette/Desktop/DRAGNet/site-info-2025-11-06.csv")
+sites <- read.csv("/Users/ingridslette/Desktop/DRAGNet/dragnet-site-coords-2026-07-08.csv")
 
 bbox <- c(left = -168, bottom = -60, right = 178, top = 78)
 myMap <- get_stadiamap(bbox, zoom = 4, maptype = "stamen_terrain_background")
@@ -23,7 +23,7 @@ ggmap(myMap) +
   geom_point(data = sites,
              aes(x = longitude, y = latitude),
              size = 3, shape = 21, 
-             color = "black", fill = "#9656a2", stroke = 0.7) +
+             color = "black", fill = "#D55E00", stroke = 0.7) +
   labs(x = "", y = "") +
   theme(axis.ticks = element_blank(),
         axis.text = element_blank())
